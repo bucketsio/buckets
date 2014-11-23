484e3e2866df0316b307eb210e6f164b5c427793
Talon Poole <talon.poole@gmail.com>
2014-11-22 23:02:30 -0700

    #235

--- bin/commands/generate.js
+++ bin/commands/generate.js
@@ -1,10 +1,31 @@
 var log = require('../utils/log');
+require('shelljs/global');
+var SKELETON_URL = 'https://github.com/bucketsio/skeleton.git';
 
 /**
  * Generate a Buckets project with a skeleton.
  *
- * @param {string} name - The name of the project.
+ * @param {string} name - Where to create the skeleton.
  */
 module.exports = function(name) {
-  log.warn('Not implemented yet. :(');
-}
+
+  /**
+   * Make sure git is installed.
+   */
+  if (!which('git')) {
+    log.warn('git is required to generate a buckets Skeleton.');
+    exit(1);
+  }
+
+  /**
+   * Clone the skeleton repo.
+   */
+  if (exec('git clone ' + SKELETON_URL + ' ' + name).code !== 0) {
+    log.error('Failed to create Buckets skeleton.');
+    exit(1);
+  }
+
+  log.info('Project', name, 'created using Buckets Skeleton.');
+  exit(0);
+
+};
